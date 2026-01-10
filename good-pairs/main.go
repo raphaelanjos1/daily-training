65b0028fda79e7a92d7b79a09f06849c2db47b1a
Raphael <raphaelanjosroa@gmail.com>
2026-01-10 19:43:14 -0300

    new solution for good pairs using hashmap

--- good-pairs/main.go
+++ good-pairs/main.go
@@ -6,15 +6,29 @@ func main() {
 }
 
 func numIdenticalPairs(nums []int) int {
-	good_pairs := 0
+	/*
+		good_pairs := 0
 
-	for index, num := range nums {
-		for i := 0; i < len(nums); i++ {
-			if num == nums[i] && index < i {
-				good_pairs += 1
+		for index, num := range nums {
+			for i := 0; i < len(nums); i++ {
+				if num == nums[i] && index < i {
+					good_pairs += 1
+				}
 			}
 		}
-	}
 
-	return good_pairs
+		return good_pairs
+		Time O(n2)
+		Space O(1)
+	*/
+	freq := make(map[int]int)
+	pairs := 0
+
+	for _, num := range nums {
+		pairs += freq[num]
+		freq[num]++
+	}
+	return pairs
+	// Time O(n)
+	// Space O(k)
 }
